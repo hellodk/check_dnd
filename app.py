a81c91a04981a5c60a63bd39218a853819a67a8a
dk <hello.dk@outlook.com>
2016-10-27 18:18:39 +0530

    fixed for flask app

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from dnd import dnd_check
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def home_page():
+    '''
+    Render home page
+    '''
+    error = None
+    data = None
+    if 'phone_number' in request.args:
+        data = {}
+        number = request.args.get('phone_number')
+        resp = json.loads(dnd_check.check_dnd(number))
+        data['phone_number'] = resp.keys()[0]
+        data['dnd_status'] = resp[resp.keys()[0]][2]
+        print data
+        return render_template('index.html', error=error, data=data)
+    else:
+        error = "Need valid number"
+        return render_template('index.html', error=error)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port=8086, debug=True)
--- dnd/dnd_check.py
+++ dnd/dnd_check.py
@@ -1,8 +1,19 @@
 import requests
 
-# curl http://ncprstatus.in/api/v1/status?numbers={9964014500,8791134412}
+# curl http://ncprstatus.in/api/v1/status?numbers={1234567890,1234566789}
 url = "http://ncprstatus.in/api/v1/status"
-payload = {"numbers": [9964014500]}
-resp = requests.get(url, params=payload)
-print resp.content
+# payload = {"numbers": [1234567890]}
+# resp = requests.get(url, params=payload)
+# print resp.content
 
+
+def check_dnd(number):
+    '''
+    Returns the dnd status
+    '''
+    payload = {'numbers': number}
+    resp = requests.get(url, params=payload)
+    if (resp.status_code == 200):
+        return resp.content
+    else:
+        return "Please check after sometime back"
