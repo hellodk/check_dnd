38d6f38fb430e7f1092d03617c19f4a4085f9521
dk <hello.dk@outlook.com>
2016-10-28 16:24:01 +0530

    Fixed minor bugs

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app -b 0.0.0.0:8086
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def home_page():
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run(host='0.0.0.0', port=8086, debug=True)
+    # app.debug = True
+    app.run(host='0.0.0.0')
